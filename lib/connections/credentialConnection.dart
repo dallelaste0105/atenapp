e7286ffdf6b50072eaf11cf4025ba45b82c07215
dallalaste <gabrieldallelaste75@gmail.com>
2025-11-18 17:00:57 -0300

    feat: função para refatoração de connections

--- lib/connections/credentialConnection.dart
+++ lib/connections/credentialConnection.dart
@@ -1,8 +1,10 @@
 import 'dart:convert';
+import 'package:Atena/main.dart';
+import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 import 'package:shared_preferences/shared_preferences.dart';
 
-const String baseUrl = "http://10.0.30.164:3000";
+final baseUrl = "http://10.0.30.164:3000";
 
 Future<void> saveTokenCredentialConnection(String token) async {
   final prefs = await SharedPreferences.getInstance();
@@ -18,6 +20,79 @@ Future<void> deleteToken() async {
   final prefs = await SharedPreferences.getInstance();
   await prefs.remove("token");
 }
+
+Future<dynamic> defaultConnection(String connectionUrl, String connectionMethod, Map<String, dynamic> connectionBody) async {
+    final jwtToken = await getToken();
+    final url = Uri.parse("http://10.0.30.164:3000$connectionUrl");
+    if (connectionMethod == "post") {
+      final res = await http.post(
+      url,
+      headers: {
+        'Content-Type': 'application/json',
+        'Authorization': 'Bearer $jwtToken',
+      },
+      body: jsonEncode({connectionBody}),
+    );
+    try{
+      final responseBody = jsonDecode(res.body);
+
+      if (res.statusCode == 200) {
+        return responseBody['message'] ?? 'Requisição realizada com sucesso.';
+      }
+      else if (res.statusCode == 401 || res.statusCode == 403) {
+        
+        await deleteToken(); 
+
+        navigatorKey.currentState?.pushNamedAndRemoveUntil(
+          '/login', (Route<dynamic> route) => false);
+        
+        return []; 
+      }
+      
+      else {
+        return responseBody['message'] ?? 'Erro na requisição.';
+      }
+    }
+    catch(error){
+      return error;
+    }
+    }
+    else if(connectionMethod == "get"){
+      final res = await http.get(
+      url,
+      headers: {
+        'Content-Type': 'application/json',
+        'Authorization': 'Bearer $jwtToken',
+      }
+    );
+    try{
+      final responseBody = jsonDecode(res.body);
+
+      if (res.statusCode == 200) {
+        return responseBody['message'] ?? 'Requisição realizada com sucesso.';
+      }
+      else if (res.statusCode == 401 || res.statusCode == 403) {
+        
+        await deleteToken(); 
+
+        navigatorKey.currentState?.pushNamedAndRemoveUntil(
+          '/login', (Route<dynamic> route) => false);
+        
+        return []; 
+      }
+      
+      else {
+        return responseBody['message'] ?? 'Erro na requisição.';
+      }
+    }
+    catch(error){
+      return error;
+    }
+    }
+    else{
+      return "A requisição não pode ser realizada";
+    }
+  }
 
 Future<String> signupCredentialConnection(
   name,
